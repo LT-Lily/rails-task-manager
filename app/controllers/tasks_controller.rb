cfb4eab661378b081b81fa9b231ebca17d50b329
Lily Tan <lilyt2995@gmail.com>
2020-09-02 23:21:12 +1000

    Imported bootstrap to application.scss and added helper  gem file simple_form to generate form with BS classes.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,41 @@
 class TasksController < ApplicationController
+   before_action :set_Task, only: [:show, :edit, :update, :destroy]
+    # READ ALL
+    def index
+        @tasks = Task.all
+    end
+    # CREATE
+    # GET
+    def new
+        @task = Task.new #params[:task]
+    end
+    # POST
+    def create
+        @task = Task.new(task_params) # create with permitted params
+        @task.save
+
+        redirect_to task_show_path(@task)
+    end
+    # READ ONE
+    def show
+    end
+
+    #UPDATE
+    def edit
+    end
+
+    def update
+        @task.update(params[:task])
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:title, :details, :completed)
+    end
+
+    def set_tasks
+        @task = Task.find(params[:id])
+    end
+    # DELETE ONE
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# READ ALL 
+  get "tasks", to: "tasks#index"
+
+# CREATE
+# GET and POST form
+  post "tasks", to: "tasks#create"
+  get "tasks/new", to: "tasks#new"
+  
+# READ ONE
+  get "tasks/:id", to: "tasks#show", as: :task_show
+# UPDATE(edit)
+  #GET form
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
 end
+
